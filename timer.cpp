c05548e5d25abace73d8017bb2e7143d05b689b9
WalkieMe <ignas.sablinskas@gmail.com>
2024-06-06 13:26:59 +0200

    Fixed some buggs related to how invalid arguments are calcaulted (it was allowed to input minutes or seconds as numbers that are higher than 59). Also, added a timer counting down functionality, where every second you see the timer going down by a single second in the terminal. The final result, it being, your message sounding out lout was not changed.

--- timer.cpp
+++ timer.cpp
@@ -3,15 +3,41 @@
 #include <iostream>
 #include <string>
 
+#define second_milliseconds 1e3
+#define minute_milliseconds 6e4
+#define hour_milliseconds 36e6
+
 using namespace std;
+
+string convertToValidFormat(long long time_left) {
+    long long hours = (time_left / hour_milliseconds);
+    long long minutes = ((long long) (time_left / minute_milliseconds)) % 60;
+    long long seconds = ((long long) (time_left / second_milliseconds)) % 60;
+    string hours_str = hours <= 9 ? "0" + to_string(hours) : to_string(hours);
+    string minutes_str = minutes <= 9 ? "0" + to_string(minutes) : to_string(minutes);
+    string seconds_str = seconds <= 9 ? "0" + to_string(seconds) : to_string(seconds);
+    return hours_str + ":" + minutes_str + ":" + seconds_str;
+}
+
+void countTime(long long sleep_time_milliseconds) {
+    long long time_left = sleep_time_milliseconds;
+    while (time_left > 0) {
+        Sleep(second_milliseconds);
+        time_left -= second_milliseconds;
+        cout << convertToValidFormat(time_left) << '\r';
+    }
+
+}
 
 /**
  * Waits for a designated amount of time and says the message
  * inputted by the user outloud
 */
-void sayMessage(int sleep_time_milliseconds, string user_message) {
-    // Wait for required time
-    Sleep(sleep_time_milliseconds);
+void sayMessage(long long sleep_time_milliseconds, string user_message) {
+
+    // This will basically display the time left (changes every second)
+    countTime(sleep_time_milliseconds);
+
     // Setup the terminal command with espeak.exe
     string command = "espeak -v en-us \"" + user_message + "\"";
     // Execute the command continuously
@@ -29,29 +55,29 @@ bool validFormat(string time_string) {
     return (time_string.size() == 8 && time_string[2] == ':' && time_string[5] == ':'
             && time_string[0] - '0' <= 9 && time_string[0] - '0' >= 0
             && time_string[1] - '0' <= 9 && time_string[1] - '0' >= 0
-            && time_string[3] - '0' <= 9 && time_string[3] - '0' >= 0
+            && time_string[3] - '0' < 6 && time_string[3] - '0' >= 0
             && time_string[4] - '0' <= 9 && time_string[4] - '0' >= 0
-            && time_string[6] - '0' <= 9 && time_string[6] - '0' >= 0
+            && time_string[6] - '0' < 6 && time_string[6] - '0' >= 0
             && time_string[7] - '0' <= 9 && time_string[7] - '0' >= 0);
 }
 
-int get_2_digit_number(string string_2_digits) {
+long long get_2_digit_number(string string_2_digits) {
     if (string_2_digits[0] == '0') {
         return string_2_digits[1] - '0';
     }
     return stoi(string_2_digits);
 }
 
-int parseTime(string time_string) {
+long long parseTime(string time_string) {
     if(!validFormat(time_string)) {
         return -1;
     }
-    int hour, min, sec, msec; // Hours, Minutes, Seconds, Milliseconds
-    hour = get_2_digit_number(time_string.substr(0, 2));
+    long long h, min, sec, msec; // Hours, Minutes, Seconds, Milliseconds
+    h = get_2_digit_number(time_string.substr(0, 2));
     min = get_2_digit_number(time_string.substr(3, 2));
     sec = get_2_digit_number(time_string.substr(6, 2));
 
-    msec = sec * 1e3 + min * 6e4 + hour * 36e5; // Calculate milliseconds
+    msec = sec * second_milliseconds + min * minute_milliseconds + h * hour_milliseconds ; // Calculate milliseconds
     return msec;
 }
 
@@ -62,7 +88,7 @@ int main(int argc, char *argv[]) {
         return 0;
     }
     string time_string(argv[1]);
-    int time = parseTime(time_string); // Parse the time
+    long long time = parseTime(time_string); // Parse the time
 
     if (time == -1) {
         invalidArgument();
@@ -70,7 +96,7 @@ int main(int argc, char *argv[]) {
     }
     string message = "Time is up"; // Default message
 
-    for (int i = 2; i < argc; i++) {
+    for (long long i = 2; i < argc; i++) {
         if ((string(argv[2]).compare("-m") == 0) && i + 1 < argc) {
             message = string(argv[++i]);
         } else {
@@ -78,7 +104,6 @@ int main(int argc, char *argv[]) {
             return 0;
         }
     }
-
     sayMessage(time, message);
 
     return 0;
